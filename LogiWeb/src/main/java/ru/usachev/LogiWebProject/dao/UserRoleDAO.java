ba5d4ba5e4fd58a580655eea6065c62698ed7ed8
alex usachev <sashulya.usachev@list.ru>
2021-06-17 23:26:49 +0300

    add readme. add javadoc comments. Delete unused methods

--- LogiWeb/src/main/java/ru/usachev/LogiWebProject/dao/UserRoleDAO.java
+++
@@ -1,7 +0,0 @@
-package ru.usachev.LogiWebProject.dao;
-
-import ru.usachev.LogiWebProject.entity.UserRole;
-
-public interface UserRoleDAO {
-    UserRole getUserRoleByUsername(String username);
-}
---
+++ LogiWeb/src/test/java/ru/usachev/LogiWebProject/test/BusinessCalculatingTest.java
@@ -0,0 +1,58 @@
+package ru.usachev.LogiWebProject.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.usachev.LogiWebProject.calculating.BusinessCalculating;
+import ru.usachev.LogiWebProject.converter.api.WaypointConverter;
+import ru.usachev.LogiWebProject.dao.api.DistanceDAO;
+import ru.usachev.LogiWebProject.dto.WaypointDTO;
+import ru.usachev.LogiWebProject.entity.Waypoint;
+import ru.usachev.LogiWebProject.service.api.OrderService;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BusinessCalculatingTest {
+    @InjectMocks
+    private BusinessCalculating businessCalculating;
+
+    @Mock
+    private OrderService orderService;
+
+    @Mock
+    private WaypointConverter waypointConverter;
+
+    @Mock
+    private DistanceDAO distanceDAO;
+
+    private List<WaypointDTO> waypointDTOList;
+
+    private List<Waypoint> waypointList;
+
+    @Before
+    public void setup(){
+        waypointDTOList = SetupClass.setupWaypointDTOList();
+        waypointList = SetupClass.setupWaypointListForBusinessCalculating();
+
+
+        when(waypointConverter.convertWaypointDTOListToWaypointList(waypointDTOList)).thenReturn(waypointList);
+    }
+
+    @Test
+    public void testCalculateNeedingCapacityByWaypointList(){
+        int capacity = businessCalculating.calculateNeedingCapacityByWaypointList(waypointDTOList);
+        Assert.assertEquals(1, capacity);
+    }
+
+    @Test
+    public void testCalculateDistanceOfOrderByOrderId(){
+
+    }
+}
